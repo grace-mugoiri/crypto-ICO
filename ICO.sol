904b81836037eed8030aded5f9f328fc1983b217
Grace Mugoiri <gracemugoiri@gmail.com>
2021-06-14 16:44:17 +0300

    add variables

--- ICO.sol
+++ ICO.sol
@@ -2,8 +2,25 @@ pragma solidity ^0.4.21;
 
 contract ICO {
     string public name;
+    string public symbol;
+    uint public decimals;
+    uint public bonusEnds;
+    uint public icoEnds;
+    uint public icoStarts;
+    uint public allContributors;
+    uint allTokens;
+    address admin;
+    mapping (address => uint) public balances;
 
     function ICO () public {
-        name = demoCoin();
+        name = "demoCoin";
+        decimals = 18;
+        symbol = "DC";
+        bonusEnds = now + 2 weeks;
+        icoEnds = now + 4 weeks;
+        icoStarts = now;
+        allTokens = 100;
+        admin = (msg.sender);
+        balances(msg.sender) = allToken;
     }
 }
